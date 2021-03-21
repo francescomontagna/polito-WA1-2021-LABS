b30fe8241ca73ea930bcd36744945df3bdaaeab2
Francesco Montagna <francesco.montagna997@gmail.com>
2021-03-21 15:56:53 +0100

    Ex01 completed

--- lab1/ex01.js
+++ lab1/ex01.js
@@ -8,21 +8,54 @@ function Task(id, description, isUrgent = false, isPrivate = false, deadline = '
     this.isUrgent = isUrgent;
     this.isPrivate = isPrivate;
 
-    this.deadline = deadline;
+    this.deadline = deadline && dayjs(deadline); // condition to format
 
-    if (this.deadline){
-        this.deadline = dayjs(deadline);
-    }
-    else {
-        this.deadline = '<not defined>';
-    }
+    this.formatDeadline = (format) => {
 
+        return this.deadline ? dayjs(deadline).format(format) : '<not defined>'
+    }
 
     this.toString = () => {
         return `Id: ${this.id}, Description: ${this.description}, Urgent: ${this.isUrgent}, `
-         + `Private: ${this.isPrivate}, Deadline: ${this.deadline.format('MMMM DD, YYYY HHA')}`;
+         + `Private: ${this.isPrivate}, Deadline: ${this.formatDeadline('MMMM DD, YYYY h:mmA')}`;
+    };
+
+}
+
+function TaskList() {
+    this.myTasks = [];
+    
+    this.add = task => this.myTasks.push(task);
+
+    this.sortAndPrint = () => {
+        const sorted = [... this.myTasks];
+
+        sorted.sort((a,b) => {
+            if (a.formatDeadline() === '<not defined>') return 1; // undefined larger
+            if (b.formatDeadline() === '<not defined>') return -1;
+
+            if (dayjs(a.deadline).isAfter(dayjs(b.deadline))) return 1;
+            
+            return -1;
+        });
+
+        sorted.forEach((el) => {
+            console.log(el.toString());
+        });
     };
 }
 
-const task = new Task(1, 'laundry', false, true, '2021-03-21T11:37:16+00:00');
-console.log(task.toString());
+const laundry = new Task(1, 'laundry', true, false, '2021-03-24T22:37:16+00:00');
+const tennis = new Task(2, 'tennis', false, true, '2021-03-22T12:00:00+00:00');
+const club = new Task(3, 'club');
+console.log(laundry.toString());
+console.log(tennis.toString());
+console.log(club.toString());
+
+let tasks = new TaskList();
+tasks.add(laundry);
+tasks.add(tennis);
+tasks.add(club);
+
+console.log();
+tasks.sortAndPrint();
